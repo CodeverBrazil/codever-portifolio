d754cb08ec9e86eaab9ab02e520a5daa50223ec3
Robert Gonçalves Batista <robert.comunicar@gmail.com>
2022-09-03 12:29:17 -0300

    Navbar header atualized

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -106,7 +106,7 @@ const Home = () => {
       </div>
 
       {/* Contact Form */}
-      <section class="bg-white">
+      <section class="bg-white" id="contact">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Tem perguntas? Eu sou todo ouvidos.</h1>
--- src/components/partials/header.jsx
+++ src/components/partials/header.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800" style={{ background: "#b359ac" }}>
-      <div x-data="{ open: false }" className="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-        <div className="p-4 flex flex-row items-center justify-between">
-          <a href="#" className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">Codever</a>
+    <header>
+      <nav class="bg-gray-900 border-gray-200 px-4 lg:px-6 py-2.5">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="https://flowbite.com" class="flex items-center">
+            <img src="img/logotipo-theme-dark.png" class="mr-3 h-6 sm:h-9" alt="Codever Logo" />
+          </a>
 
-          <button className="md:hidden rounded-lg focus:outline-none focus:shadow-outline">
-            <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
-              <path x-show="!open" fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd"></path>
-              <path x-show="open" fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
-            </svg>
-          </button>
+          <div class="grid items-center lg:order-2">
+            <a href="tel:+5522999363638" class="text-white bg-primary-700 font-medium rounded-lg text-sm">+55 (22) 99936-3638</a>
+            <a href="tel:+5522999363638" class="text-white bg-primary-700 font-medium rounded-lg text-sm">+55 (22) 99936-3638</a>
+          </div>
+
+          <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+            <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <a href="#about" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Sobre</a>
+              </li>
+              <li>
+                <a href="#skills" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Habilidades</a>
+              </li>
+              <li>
+                <a href="#contact" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0" aria-current="page">Contato</a>
+              </li>
+            </ul>
+          </div>
         </div>
-
-        <nav className="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#about">About</a>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#skills">Skills</a>
-          <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#contact">Contact</a>
-        </nav >
-      </div>
+      </nav>
     </header>
   )
 }
