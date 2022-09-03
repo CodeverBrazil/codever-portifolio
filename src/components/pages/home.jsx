fa945d29382764c6df293d76f0382e33a68b6deb
Robert Gonçalves Batista <robert.comunicar@gmail.com>
2022-09-03 12:07:14 -0300

    Contact form created

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -30,13 +30,13 @@ const Home = () => {
       </div>
 
       {/* Sobre */}
-      <div className="bg-gray-50 dark:bg-gray-900" id="about">
+      <div className="bg-gray-900" id="about">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">Powering innovation at <span className="font-extrabold">200,000+</span> companies worldwide</h2>
+          <div className="max-w-screen-lg text-gray-500 sm:text-lg">
+            <h2 className="mb-4 text-4xl font-bold text-gray-900">Powering innovation at <span className="font-extrabold">200,000+</span> companies worldwide</h2>
             <p className="mb-4 font-light">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
             <p className="mb-4 font-medium">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
-            <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
+            <a href="#" className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800">
               Learn more
               <svg className="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
             </a>
@@ -104,6 +104,41 @@ const Home = () => {
           anchorLink={'#'}
           style={{ background: "#212121", padding: "0.5rem" }} />
       </div>
+
+      {/* Contact Form */}
+      <section class="bg-white">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Tem perguntas? Eu sou todo ouvidos.</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl">Sinta-se à vontade para entrar em contato conosco para quaisquer perguntas ou comentários que você possa ter. Estamos prontos para ouvi-lo e responder às suas perguntas.</p>
+          </div>
+
+          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <div class="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8">
+              <form class="space-y-6" action="#">
+                <h5 class="text-xl font-medium text-gray-900">Fale conosco</h5>
+
+                <div>
+                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Seu nome</label>
+                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5" placeholder="John Doe" required="" />
+                </div>
+
+                <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Seu email</label>
+                  <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5" placeholder="john.doe@contoso.com" required="" />
+                </div>
+
+                <div>
+                  <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Sua mensagem</label>
+                  <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500" placeholder="Your message..."></textarea>
+                </div>
+
+                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
 
       {/* Rodapé */}
       <Footer />
